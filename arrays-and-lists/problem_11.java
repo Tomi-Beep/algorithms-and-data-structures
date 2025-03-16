b6ae28f5ca173e75703da760298ef3f103868e92
Tomi Bogoeski <81703403+Tomi-Beep@users.noreply.github.com>
2025-03-16 23:50:18 +0100

    Create problem_11.java

---
+++ arrays-and-lists/problem_11.java
@@ -0,0 +1,514 @@
+import java.util.Scanner;
+
+class DLL<E> {
+    private DLLNode<E> first, last;
+
+    public DLL() {
+        // Construct an empty SLL
+        this.first = null;
+        this.last = null;
+    }
+
+    public void insertFirst(E o) {
+        DLLNode<E> ins = new DLLNode<E>(o, null, first);
+        if (first == null)
+            last = ins;
+        else
+            first.pred = ins;
+        first = ins;
+    }
+
+    public void insertLast(E o) {
+        if (first == null)
+            insertFirst(o);
+        else {
+            DLLNode<E> ins = new DLLNode<E>(o, last, null);
+            last.succ = ins;
+            last = ins;
+        }
+    }
+
+    public void insertAfter(E o, DLLNode<E> after) {
+        if (after == last) {
+            insertLast(o);
+            return;
+        }
+        DLLNode<E> ins = new DLLNode<E>(o, after, after.succ);
+        after.succ.pred = ins;
+        after.succ = ins;
+    }
+
+    public void insertBefore(E o, DLLNode<E> before) {
+        if (before == first) {
+            insertFirst(o);
+            return;
+        }
+        DLLNode<E> ins = new DLLNode<E>(o, before.pred, before);
+        before.pred.succ = ins;
+        before.pred = ins;
+    }
+
+    public E deleteFirst() {
+        if (first != null) {
+            DLLNode<E> tmp = first;
+            first = first.succ;
+            if (first != null) first.pred = null;
+            if (first == null)
+                last = null;
+            return tmp.element;
+        } else
+            return null;
+    }
+
+    public E deleteLast() {
+        if (first != null) {
+            if (first.succ == null)
+                return deleteFirst();
+            else {
+                DLLNode<E> tmp = last;
+                last = last.pred;
+                last.succ = null;
+                return tmp.element;
+            }
+        } else
+            return null;
+    }
+
+    public E delete(DLLNode<E> node) {
+        if (node == first) {
+            return deleteFirst();
+        }
+        if (node == last) {
+            return deleteLast();
+        }
+        node.pred.succ = node.succ;
+        node.succ.pred = node.pred;
+        return node.element;
+
+    }
+
+    public DLLNode<E> find(E o) {
+        if (first != null) {
+            DLLNode<E> tmp = first;
+            while (!tmp.element.equals(o) && tmp.succ != null)
+                tmp = tmp.succ;
+            if (tmp.element.equals(o)) {
+                return tmp;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+            }
+        } else {
+            System.out.println("Listata e prazna");
+        }
+        return null;
+    }
+
+    public void deleteList() {
+        first = null;
+        last = null;
+    }
+
+    public int getSize() {
+        int listSize = 0;
+        DLLNode<E> tmp = first;
+        while (tmp != null) {
+            listSize++;
+            tmp = tmp.succ;
+        }
+        return listSize;
+    }
+
+    @Override
+    public String toString() {
+        String ret = new String();
+        if (first != null) {
+            DLLNode<E> tmp = first;
+            ret += tmp.toString();
+            while (tmp.succ != null) {
+                tmp = tmp.succ;
+                ret += "<->" + tmp.toString();
+            }
+        } else
+            ret = "Prazna lista!!!";
+        return ret;
+    }
+
+    public String toStringR() {
+        String ret = new String();
+        if (last != null) {
+            DLLNode<E> tmp = last;
+            ret += tmp.toString();
+            while (tmp.pred != null) {
+                tmp = tmp.pred;
+                ret += "<->" + tmp.toString();
+            }
+        } else
+            ret = "Prazna lista!!!";
+        return ret;
+    }
+
+    public DLLNode<E> getFirst() {
+        return first;
+    }
+
+    public DLLNode<E> getLast() {
+
+        return last;
+    }
+
+    public void mirror() {
+
+        DLLNode<E> tmp = null;
+        DLLNode<E> current = first;
+        last = first;
+        while (current != null) {
+            tmp = current.pred;
+            current.pred = current.succ;
+            current.succ = tmp;
+            current = current.pred;
+        }
+
+        if (tmp != null && tmp.pred != null) {
+            first = tmp.pred;
+        }
+    }
+}
+
+class DLLNode<E> {
+    protected E element;
+    protected DLLNode<E> pred, succ;
+
+    public DLLNode(E elem, DLLNode<E> pred, DLLNode<E> succ) {
+        this.element = elem;
+        this.pred = pred;
+        this.succ = succ;
+    }
+
+    @Override
+    public String toString() {
+        return element.toString();
+    }
+}
+
+class SLL<E> {
+    private SLLNode<E> first;
+
+    public SLL() {
+        // Construct an empty SLL
+        this.first = null;
+    }
+
+    public void deleteList() {
+        first = null;
+    }
+
+    public int size() {
+        int listSize = 0;
+        SLLNode<E> tmp = first;
+        while (tmp != null) {
+            listSize++;
+            tmp = tmp.succ;
+        }
+        return listSize;
+    }
+
+    @Override
+    public String toString() {
+        String ret = new String();
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            ret += tmp.element;
+            while (tmp.succ != null) {
+                tmp = tmp.succ;
+                ret += "->" + tmp.element;
+            }
+        } else
+            ret = "Prazna lista!!!";
+        return ret;
+    }
+
+    public void insertFirst(E o) {
+        SLLNode<E> ins = new SLLNode<E>(o, null);
+        ins.succ = first;
+        //SLLNode<E> ins = new SLLNode<E>(o, first);
+        first = ins;
+    }
+
+    public void insertAfter(E o, SLLNode<E> node) {
+        if (node != null) {
+            SLLNode<E> ins = new SLLNode<E>(o, node.succ);
+            node.succ = ins;
+        } else {
+            System.out.println("Dadenot jazol e null");
+        }
+    }
+
+    public void insertBefore(E o, SLLNode<E> before) {
+
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            if (first == before) {
+                this.insertFirst(o);
+                return;
+            }
+            //ako first!=before
+            while (tmp.succ != before && tmp.succ != null)
+                tmp = tmp.succ;
+            if (tmp.succ == before) {
+                tmp.succ = new SLLNode<E>(o, before);
+                ;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+            }
+        } else {
+            System.out.println("Listata e prazna");
+        }
+    }
+
+    public void insertLast(E o) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            while (tmp.succ != null)
+                tmp = tmp.succ;
+            tmp.succ = new SLLNode<E>(o, null);
+        } else {
+            insertFirst(o);
+        }
+    }
+
+    public E deleteFirst() {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            first = first.succ;
+            return tmp.element;
+        } else {
+            System.out.println("Listata e prazna");
+            return null;
+        }
+    }
+
+    public E delete(SLLNode<E> node) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            if (first == node) {
+                return this.deleteFirst();
+            }
+            while (tmp.succ != node && tmp.succ.succ != null)
+                tmp = tmp.succ;
+            if (tmp.succ == node) {
+                tmp.succ = tmp.succ.succ;
+                return node.element;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+                return null;
+            }
+        } else {
+            System.out.println("Listata e prazna");
+            return null;
+        }
+
+    }
+
+    public SLLNode<E> getFirst() {
+        return first;
+    }
+
+    public SLLNode<E> find(E o) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            while (!tmp.element.equals(o) && tmp.succ != null)
+                tmp = tmp.succ;
+            if (tmp.element.equals(o)) {
+                return tmp;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+            }
+        } else {
+            System.out.println("Listata e prazna");
+        }
+        return null;
+    }
+
+    public void merge(SLL<E> in) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            while (tmp.succ != null)
+                tmp = tmp.succ;
+            tmp.succ = in.getFirst();
+        } else {
+            first = in.getFirst();
+        }
+    }
+
+    public void mirror() {
+        if (first != null) {
+            //m=nextsucc, p=tmp,q=next
+            SLLNode<E> tmp = first;
+            SLLNode<E> newsucc = null;
+            SLLNode<E> next;
+
+            while (tmp != null) {
+                next = tmp.succ;
+                tmp.succ = newsucc;
+                newsucc = tmp;
+                tmp = next;
+            }
+            first = newsucc;
+        }
+    }
+}
+
+class SLLNode<E> {
+    protected E element;
+    protected SLLNode<E> succ;
+
+    public SLLNode(E elem, SLLNode<E> succ) {
+        this.element = elem;
+        this.succ = succ;
+    }
+}
+
+
+@SuppressWarnings("unchecked")
+class Array<E> {
+    private E data[]; // declared to be an Object since it would be too
+    // complicated with generics
+    private int size;
+
+    public Array(int capacity) {
+        this.data = (E[]) new Object[capacity];
+        this.size = 0;
+    }
+
+    public void insertLast(E o) {
+        //check if there is enough capacity, and if not - resize
+        if (size + 1 > data.length)
+            this.resize();
+        data[size++] = o;
+    }
+
+    public void insert(int position, E o) {
+        // before all we check if position is within range
+        if (position >= 0 && position <= size) {
+            //check if there is enough capacity, and if not - resize
+            if (size + 1 > data.length)
+                this.resize();
+            //copy the data, before doing the insertion
+            for (int i = size; i > position; i--) {
+                data[i] = data[i - 1];
+            }
+            data[position] = o;
+            size++;
+        } else {
+            System.out.println("Ne mozhe da se vmetne element na taa pozicija");
+        }
+    }
+
+    public void set(int position, E o) {
+        if (position >= 0 && position < size)
+            data[position] = o;
+        else
+            System.out.println("Ne moze da se vmetne element na dadenata pozicija");
+    }
+
+    public E get(int position) {
+        if (position >= 0 && position < size)
+            return data[position];
+        else
+            System.out.println("Ne e validna dadenata pozicija");
+        return null;
+    }
+
+    public int find(E o) {
+        for (int i = 0; i < size; i++) {
+            if (o.equals(data[i]))
+                return i;
+        }
+        return -1;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void delete(int position) {
+        // before all we check if position is within range
+        if (position >= 0 && position < size) {
+            // first resize the storage array
+            E[] newData = (E[]) new Object[size - 1];
+            // copy the data prior to the delition
+            for (int i = 0; i < position; i++)
+                newData[i] = data[i];
+            // move the data after the deletion
+            for (int i = position + 1; i < size; i++)
+                newData[i - 1] = data[i];
+            // replace the storage with the new array
+            data = newData;
+            size--;
+        }
+    }
+
+    public void resize() {
+        // first resize the storage array
+        E[] newData = (E[]) new Object[size * 2];
+        // copy the data
+        for (int i = 0; i < size; i++)
+            newData[i] = data[i];
+        // replace the storage with the new array
+        this.data = newData;
+    }
+
+    @Override
+    public String toString() {
+        String ret = new String();
+        if (size > 0) {
+            ret = "{";
+            ret += data[0];
+            for (int i = 1; i < size; i++) {
+                ret += "," + data[i];
+            }
+            ret += "}";
+            return ret;
+        } else {
+            ret = "Prazna niza!";
+        }
+        return ret;
+    }
+
+}
+
+public class chillinnn {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        DLL<Integer> lista = new DLL<Integer>();
+        for (int i = 0; i < n; i++) {
+            lista.insertLast(sc.nextInt());
+        }
+        int counter = 0;
+        DLLNode<Integer> t = lista.getFirst().succ;
+        while (t != null && t.pred != null && t.succ != null) {
+            DLLNode<Integer> l = t.pred;
+            DLLNode<Integer> r = t.succ;
+            int zbir1 = l.element;
+            int zbir2 = r.element;
+            int counter1 = 1;
+            int counter2 = 1;
+            while (l != null && l.pred != null) {
+                l = l.pred;
+                zbir1 += l.element;
+                counter1++;
+            }
+            while (r != null && r.succ != null) {
+                r = r.succ;
+                zbir2 += r.element;
+                counter2++;
+            }
+            if ((float) zbir1 / counter1 > (float) zbir2 / counter2)
+                counter++;
+            t = t.succ;
+        }
+        System.out.println((counter));
+    }
+}
